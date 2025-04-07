c6cfcc436a027074e17e3ba800ef3d9ff4dd5e09
Alex Ou <oualex0312@gmail.com>
2025-04-07 17:56:21 -0400

    Add files via upload

---
+++ ouxx5748_l04/src/cp213/ArraysTest.java
@@ -0,0 +1,151 @@
+package cp213;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Arrays Lab task methods.
+ *
+ * @author Zi Feng (Alex) Ou
+ * @version 2022-02-09
+ */
+public class ArraysTest {
+
+    /**
+     * Tests arrays.
+     *
+     * @param args unused default parameter
+     */
+    public static void main(final String[] args) {
+	System.out.println("Task 1");
+	System.out.println(ArraysTest.task1());
+	System.out.println("Task 2");
+	System.out.println(ArraysTest.task2());
+	System.out.println("Task 3");
+	System.out.println(ArraysTest.task3());
+	System.out.println("Task 4");
+	System.out.println(ArraysTest.task4());
+	System.out.println("Task 5");
+	System.out.println(Arrays.toString(ArraysTest.task5()));
+    }
+
+    /**
+     * Print the contents of the arrays first and second using a standard for loop.
+     *
+     * @return true if second contains the same values as first, false otherwise
+     */
+    public static boolean task1() {
+	final int[] first = { 1, 3, 5, 7, 9 };
+	final int[] second = first;
+
+	// your code here
+
+	System.out.println("Values in first:");
+	for (int i = 0; i < first.length; i++) {
+	    System.out.println(first[i]);
+	}
+
+	System.out.println("Values in second:");
+	for (int i = 0; i < second.length; i++) {
+	    System.out.println(second[i]);
+	}
+
+	// return true; - or - return false;
+
+	return true;
+
+    }
+
+    /**
+     * Double the contents of the array first with a standard for loop.
+     *
+     * @return true if second contains the same values as first, false otherwise
+     */
+    public static boolean task2() {
+	final int[] first = { 1, 3, 5, 7, 9 };
+	final int[] second = first;
+
+	// your code here
+
+	boolean check = true;
+
+	for (int i = 0; i < first.length; i++) {
+	    first[i] = first[i] * 2;
+	}
+	for (int i = 0; i < second.length; i++) {
+	    if (second[i] != first[i]) {
+		check = false;
+	    }
+	}
+
+	// return true; - or - return false;
+
+	return check;
+    }
+
+    /**
+     * Double the contents of the array first with an enhanced for loop.
+     *
+     * @return true if the values in first are permanently changed, false otherwise
+     */
+    public static boolean task3() {
+	final int[] first = { 1, 3, 5, 7, 9 };
+
+	// your code here
+
+	boolean check = false;
+
+	for (int i : first) {
+	    i *= 2;
+	}
+	for (int i = 0; i < first.length; i++) {
+	    if (first[i] != (i * 2) + 1) {
+		check = true;
+	    }
+	}
+
+	// return true; - or - return false;
+
+	return check;
+
+    }
+
+    /**
+     * Attempt to assign the array first to a row of the 2D array third.
+     *
+     * @return true if this is allowed, false otherwise
+     */
+    public static boolean task4() {
+	final int[] first = { 1, 3, 5, 7, 9 };
+	final int[][] third = new int[1][first.length];
+
+	// your code here
+
+	third[0] = first;
+
+	// return true; - or - return false;
+
+	return third[0] == first;
+
+    }
+
+    /**
+     * Assign the values 0 through 9 to an Integer ArrayList.
+     *
+     * @return the contents of the ArrayList as an Integer[] array.
+     */
+    public static Integer[] task5() {
+	final ArrayList<Integer> values = new ArrayList<>();
+
+	// your code here
+
+	for (int i = 0; i < 10; i++) {
+	    values.add(i);
+	}
+
+	Integer[] arrList = values.toArray(new Integer[0]);
+
+	return arrList;
+
+    }
+}
